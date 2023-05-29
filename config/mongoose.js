f432dce748a147ecc4a979df25082e249db79e31
Sayyadsulthan <105346556+Sayyadsulthan@users.noreply.github.com>
2023-05-29 13:30:34 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(`mongodb+srv://asulthan088:Asulthan088@habit-tracker-cluster.sump3er.mongodb.net/?retryWrites=true&w=majority`)
+    .connect(` mongodb+srv://asulthan088:Asulthan088@habit-tracker-cluster.sump3er.mongodb.net/habit-tracker-skillTest?retryWrites=true&w=majority`)
     .then(()=>console.log("db connectionsuccesfull.."))
     .catch((err)=>console.log("err in db: ", err))
 
